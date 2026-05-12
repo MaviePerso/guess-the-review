2a616804326ad20f18e069c9221d3ede03c27fe8
Mira <mira@example.com>
2026-05-12 19:39:50 +0200

    Test staging: modified homepage title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ function HomeContent() {
   return (
     <div className="container">
       <div className="header">
-        <h1 className="title">Guess The <span>Review</span></h1>
+        <h1 className="title">Guess The <span>Review (STAGING)</span></h1>
         <p className="subtitle">Devine la note globale ou le prix des pires et meilleurs objets du net.</p>
       </div>
 
